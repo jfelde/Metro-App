d7191332ea0eb8ba87d42f41dbb86426cdde3478
John Felde <jwfelde@gmail.com>
2015-01-06 19:43:03 -0500

    actually added /Data

---
+++ Data/make_stations_dict.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import sys,os
+import subprocess
+import urllib2
+import time
+import cPickle
+
+stations = []
+for i in range(0,115):
+    url_str = "http://www.wmata.com/rider_tools/pids/showpid.cfm?station_id=" + str(i)
+    url = urllib2.urlopen(url_str)
+    lines = url.readlines()
+    station_name = lines[5].strip()[7:-8]
+    if station_name != '':
+        stations.append([i,station_name,url_str])
+    time.sleep(0.1)
+
+stations_file = open("./stations.pickle","w")
+cPickle.dump(stations,stations_file)
